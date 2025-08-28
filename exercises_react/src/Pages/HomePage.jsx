b021bcefd61d8241d081c572892d0e25ce671c5d
Christina Rusu <rusuc@oregonstate.edu>
2025-08-27 19:20:58 -0700

    Update HomePage.jsx

--- exercises_react/src/Pages/HomePage.jsx
+++ exercises_react/src/Pages/HomePage.jsx
@@ -3,9 +3,7 @@
 import {useNavigate} from 'react-router-dom';
 import {useEffect, useState} from 'react';
 import ExerciseTable from '../Components/ExerciseTable';
-const API_URL = 'https://full-stack-mern-app-exercise-tracker-backup-kcpdxz7jp.vercel.app';
-
-
+const API_URL = import.meta.env.VITE_API_URL;
 
 
 function HomePage({setExercise}) {
