98a292000fb09151321e3fb4c1fb0b2c3423bd3a
Christina Rusu <rusuc@oregonstate.edu>
2025-08-27 20:08:42 -0700

    Update vite.config.js

--- exercises_react/vite.config.js
+++ exercises_react/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 })
 
 // src/api.js or wherever you call your backend
-const BACKEND_URL = 'https://full-stack-mern-app-exercise-tracke.vercel.app';
+const BACKEND_URL = 'full-stack-mern-app-exercise-tracker-backup-hvn2zklip.vercel.app';
 
 export async function getExercises() {
   const response = await fetch(`${BACKEND_URL}/exercises`);
